69c9f94bf6a833c809633eb72202310bad58e772
MonalisaBasa <monalisa.basa.mb@gmail.com>
2023-04-03 21:26:32 +0530

    MODIFIED ELEMENTS BY CLASSNAME

--- dom.js
+++ dom.js
@@ -25,3 +25,12 @@
 var heading=document.getElementsByClassName('title');
 heading[0].style.color="Green";
 heading[0].style.fontWeight='bold';
+
+//GETELEMENTBYCLASSNAME//
+
+var items=document.getElementsByClassName("list-group-item");
+items[2].style.backgroundColor="green";
+for(var i=0;i<items.length;i++)
+{
+    items[i].style.fontWeight="bold";
+}
