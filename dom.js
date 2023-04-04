ef707bae2aad5ed576380c825501a119ed5be07c
MonalisaBasa <monalisa.basa.mb@gmail.com>
2023-04-04 20:18:36 +0530

    Modified using GetElement by Tagname

--- dom.js
+++ dom.js
@@ -28,9 +28,11 @@ heading[0].style.fontWeight='bold';
 
 //GETELEMENTBYCLASSNAME//
 
-var items=document.getElementsByClassName("list-group-item");
-items[2].style.backgroundColor="green";
-for(var i=0;i<items.length;i++)
-{
-    items[i].style.fontWeight="bold";
-}
+// var items=document.getElementsByClassName("list-group-item");
+// items[2].style.backgroundColor="green";
+// for(var i=0;i<items.length;i++)
+// {
+//     items[i].style.fontWeight="bold";
+// }
+document.getElementsByClassName("list")[0].innerHTML="Item No.5"
+document.getElementsByTagName("li")[4].innerText="Item No.5"
