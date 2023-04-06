a95e67af76b025c82795aef064f410bf47fcf340
MonalisaBasa <monalisa.basa.mb@gmail.com>
2023-04-07 00:20:06 +0530

    DELETE AND EDIT FUNCTIONALITY

--- dom.js
+++ dom.js
@@ -40,35 +40,100 @@
 ;
 
 //QUERYSELECTOR//
-var items=document.getElementsByClassName("list-group-item");
-items[1].style.backgroundColor="green"
-var secondItem=document.querySelectorAll('.list-group-item');
-secondItem[1].style.color="Green";
-var odd=document.querySelectorAll('li:nth-child(odd)');
-for(var i=0;i<odd.length;i++)
-{
-    odd[i].style.backgroundColor="#ccc";
-}
+// var items=document.getElementsByClassName("list-group-item");
+// items[1].style.backgroundColor="green"
+// var secondItem=document.querySelectorAll('.list-group-item');
+// secondItem[1].style.color="Green";
+// var odd=document.querySelectorAll('li:nth-child(odd)');
+// for(var i=0;i<odd.length;i++)
+// {
+//     odd[i].style.backgroundColor="#ccc";
+// }
 
 //INSERTBEFORE//
 //Before header
-var newDiv=document.createElement("div");
-var newDivText=document.createTextNode("Hello World");
-newDiv.appendChild(newDivText);
+// var newDiv=document.createElement("div");
+// var newDivText=document.createTextNode("Hello World");
+// newDiv.appendChild(newDivText);
 
 
-var container=document.querySelector('header .container');
-var h1=document.querySelector('header h1')
-console.log(container.insertBefore(newDiv,h1));
+// var container=document.querySelector('header .container');
+// var h1=document.querySelector('header h1')
+// console.log(container.insertBefore(newDiv,h1));
 
-//Before Item 1
-var newDiv1=document.createElement("div");
-var newDivText=document.createTextNode("Hello World");
-newDiv1.appendChild(newDivText);
+// //Before Item 1
+// var newDiv1=document.createElement("div");
+// var newDivText=document.createTextNode("Hello World");
+// newDiv1.appendChild(newDivText);
 
-var list=document.getElementById("items");
-list.insertBefore(newDiv1,list.children[0]);
+// var list=document.getElementById("items");
+// list.insertBefore(newDiv1,list.children[0]);
+
+var form=document.getElementById('addform');
+console.log(form);
+//ul tag
+var item=document.getElementById('items');
+
+//form submit Event
+form.addEventListener('submit',addItem);
+
+//Delete event 
+item.addEventListener('click',removeItem)
+//Add Item
+function addItem(e)
+{
+    e.preventDefault();
+
+    //Get Input value 
+    var newItem=document.getElementById('item').value;
+
+    //create new li element
+    var li=document.createElement('li');
+
+    //Add class
+     li.className='list-group-item';
+
+     //Add textNode with input value
+
+     li.appendChild(document.createTextNode(newItem));
+
+     //create del button element
+     var button=document.createElement('button');
+
+     //Add class to del button
+     button.className='btn btn-danger btn-sm float-right delete';
+
+     //Add TextNode
+     button.appendChild(document.createTextNode('X'));
+     //Append button to li
+     li.appendChild(button);
+     
+     //Append li to ul
+
+     item.appendChild(li);
 
 
+}
 
+//Remove Item
+function removeItem(e)
+{
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are you sure?'))
+    {
+        var li=e.target.parentElement;
+        item.removeChild(li);
+    }
+  }
+}
+//Add Edit button
 
+// var edit=document.createElement('button');
+
+// edit.appendChild(document.createTextNode('Edit'));
+
+// for(var i=0;i<item.length;i++)
+// {
+//     .appendChild(edit);
+
+// }
